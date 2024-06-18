ded6ab0b6dddb4a8911d72b387022fb2e38b11e9
kangeunsong <dmxthd@naver.com>
2024-06-18 10:28:41 +0900

    회원정보 수정 DB 연결

--- ows T/js/firebase.js
+++ ows T/js/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAuth, updatePassword, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getDatabase, ref, set, get} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 import { v4 as uuidv4 } from "https://cdn.jsdelivr.net/npm/uuid@9.0.0/dist/esm-browser/index.js";
 
@@ -17,7 +17,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 
-export { auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword, ref, set, get, uuidv4 };
+export { auth, db, updatePassword, createUserWithEmailAndPassword, signInWithEmailAndPassword, ref, set, get, uuidv4 };
 
 export async function getUserInfo(uid) {
     try {
