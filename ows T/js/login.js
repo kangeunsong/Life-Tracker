64a60b55aee50725e4d5108eeb959cf8583f0aa8
kangeunsong <dmxthd@naver.com>
2024-06-18 14:22:21 +0900

    로그인 성공 메시지창에 사용자 이름 추가

--- ows T/js/login.js
+++ ows T/js/login.js
@@ -1,12 +1,31 @@
-import { auth, signInWithEmailAndPassword } from './firebase.js';
+import { auth, db, ref, get, getUserInfo, signInWithEmailAndPassword } from './firebase.js';
 
 async function login(email, password) {
     try {
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
-        alert('로그인 성공');
-        window.location.href = "scheduler.html";
+
+        let username;
+        const user = userCredential.user;
+        
+        try {
+            const userInfo = await getUserInfo(user.uid);
+            const userUid = userInfo.uid;
+            const path = `users/${userUid}`;
+            const snapshot = await get(ref(db, path));
+    
+            if (snapshot.exists()) {
+                const data = snapshot.val();
+                username = data.name;
+            }
+
+            alert(`로그인 성공\n\n${username}님 안녕하세요!`);
+            window.location.href = "scheduler.html";
+        } catch (error) {
+            console.error(error);
+        }
+        
     } catch (error) {
-        alert('로그인 실패: ' + error.message);
+        alert('로그인 실패\n\n이메일과 비밀번호를 확인해주세요.');
     }
 }
 
